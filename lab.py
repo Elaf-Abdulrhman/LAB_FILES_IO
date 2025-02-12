8047b3a661c37ffed5c8f3eae9a126fb70e659a2
elaf <elafabdualrahman00@gmail.com>
2025-02-12 13:54:47 +0300

    edited

--- lab.py
+++ lab.py
@@ -8,7 +8,7 @@
 '''
 
 while True:
-    user_input = input("\nWelcome this is the to-do program \nDo you want to add a new To-Do item?\nAnswer by y for yes, n for no, exit to end the program: ")
+    user_input = input("\nWelcome this is the to-do program, Do you want to add a new To-Do item?\nAnswer by y for yes, n for no, exit to end the program: ")
     
     if user_input.lower() == "y":
         item_name = input("Enter your new To-Do item: ")
