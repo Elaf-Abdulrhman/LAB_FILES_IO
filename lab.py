b530a978a76cfe07c63122ebda2ea59156bc1820
elaf <elafabdualrahman00@gmail.com>
2025-02-12 12:32:43 +0300

    created while loop and asked the user 2 questions

---
+++ lab.py
@@ -0,0 +1,19 @@
+'''
+
+## Using what you learned about Python File I/O , we want to make a progeram called To-Do List , this program should do the following:
+- Ask the user do you want to add a new To-Do item? answer by "y" for yes and "n" for no.
+- If the user answers yes , then ask the user to type in his new To-Do item . Then save that To-Do item inside the a file to_do.txt on a new line.
+- If the user answers no, then ask the user : do you want to list your To-Do items ? answer "y" for yes and "n" for no. 
+- If the user answers yes for reading his To-Do list , then print a list of the To-Do items one item per line.
+- Then return again to ther first question and ask again, you coninue this untill the user types in "exit" , then you exit the program. and print to the user "thank you for using the To-Do program, come back again soon"
+'''
+
+while True:
+    add_item = input("do you want to add a new To-Do item? answer by y for yes and n for no ")
+    if add_item.lower() == "y":
+        item_name = input("Enter your new To-Do item: ")
+        new_item = open("to_do.txt", "w", encoding="UTF-8")
+        new_item.write()
+        
+        
+        
